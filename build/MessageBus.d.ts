81402ea9eac55468e67cf9a667ddd5d772de55d5
Ilia Choly <ilia.choly@gmail.com>
2015-03-11 12:44:39 -0400

    add build artifacts to repo

---
+++ build/MessageBus.d.ts
@@ -0,0 +1,55 @@
+declare module MessageBus {
+    interface Endpoint {
+        postMessage(message: any): void;
+        addEventListener(type: string, listener: (ev: any) => any): any;
+        removeEventListener(type: string, listener: (ev: any) => any): any;
+    }
+    interface Listener {
+        (payload?: any): any;
+    }
+    class MessageBus {
+        private endpoint;
+        private channels;
+        private unlisten;
+        /**
+         * Wrap an `Endpoint` and provide a pubsub interface.
+         *
+         * @param endpoint WebWorker endpoint
+         */
+        constructor(endpoint: Endpoint);
+        /**
+         * Remove event listeners from supplied `Endpoint`
+         * Delete all listeners from `MessageBus`
+         */
+        close(): void;
+        /**
+         * Listener on channel
+         *
+         * @param name Channel to listen on
+         * @param listener Callback function
+         */
+        on(name: string, listener: Listener): void;
+        /**
+         * Unlisten from channel
+         *
+         * @param name Channel to unlisten
+         * @param listener Callback function
+         */
+        off(name: string, listener: Listener): void;
+        /**
+         * Emit a message on a channel
+         *
+         * @param name Channel name
+         * @param payload Message data
+         */
+        emit(name: string, payload?: any): void;
+        private channelExists(name);
+        private onEvent(name, payload);
+    }
+    /**
+     * Factory function for creating a `MessageBus`
+     *
+     * @param endpoint WebWorker endpoint
+     */
+    function create(endpoint: Endpoint): MessageBus;
+}
